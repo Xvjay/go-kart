e21b3da2067d5a900e28748c511b96d734e75286
Christopher Ramos <chrisjay123890@gmail.com>
2024-06-24 14:20:56 -0400

    added more stuff to the register page

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,60 +1,69 @@
 import {NextResponse} from "next/server";
 import pool from "@/app/libs/mysql";
 
-export async function GET() {
-    try {
-        const db = await pool.getConnection()
-        const query = 'SELECT Name FROM users WHERE Id = 1';
-        const [rows] = await db.execute(query)
-        db.release()
-
-        return NextResponse.json(rows)
-    } catch (error) {
-        return NextResponse.json({
-            error: error
-        }, {status: 500})
-    }
-}
 
 export async function POST(request : Request) {
     const db = await pool.getConnection();
-    const {Name, Email, Password} = await request.json();
+    const {Name, Email, Password , Password1} = await request.json();
 
     try {
         const query1 = 'Select Email from users where Email = (?)'
-        const [result1] : [any[], any[]] = await db.execute(query1, [Email])
-        const res2 = result1.map(row => row.Email);
-        console.log(res2)
+        const [result1] : any = await db.execute(query1, [Email])
+        const res2 = result1.map((row: { Email: any; }) => row.Email);
+        console.log(res2[0].length)
+        if(res2 == ""){
+            return NextResponse.json({
+                message: "Enter an Email"
+            }, {status: 201});
+        }
+       
 
-        if (res2[0].length > 0) {
-            console.log("yes")
-        } else {
-            console.log("no")
-            // const query2 = 'Select Name from users where Name = (?)'
-            // const [user] : [any[], any[]] = await db.execute(query2, [Name])
-            // const user1 = user.map(row => row.Name)
-            // console.log(user1[0])
+        return NextResponse.json({
+            message: "This Email has already been used"
+        }, {status: 201});
 
-            // if (user1[0] > 1) {
-            //     console.log("yes")
-            // } else {
-            //     console.log("no")
-            // }
+    } catch {
+        try{
+            const query1 = 'Select Name from users where Name = (?)'
+            const [res] : any = await db.execute(query1, [Name])
+            const res2 = res.map((row: { Name: any }) => row.Name);
+
+            console.log(res2[0].length)
+
+            if(res2 == ""){
+                return NextResponse.json({
+                    message: "Enter a Username"
+                }, {status: 201});
+
+
+            }
+            return NextResponse.json({
+                message: "This Username has already been used"
+            }, {status: 201});
+
+        }catch{
+            console.log(Password1)
+            console.log(Password)
+            if(Password == "" || Password1 == ""){
+                return NextResponse.json({ message: "Please fill out both Passwords" }, { status: 201 });
+
+
+            }
+            if(Password == Password1 && Password != "" || Password1 != ""){
+                console.log("yes")
+                const query = 'INSERT INTO users (Name, Email, Password) VALUES (?, ?, ?)';
+                const [result] = await db.execute(query, [Name, Email, Password]);
+                return NextResponse.json({ message: "User created successfully" }, { status: 200 });
+
+
+            }else{
+                console.log("Paswords are not the same")               
+                 return NextResponse.json({ message: "Passwords are not the same" }, { status: 201 });
+
+            }
+           
 
         }
 
-    } catch (error) {
-        return NextResponse.json({
-            message: "hello"
-        }, {status: 303})
-
     }
-
-    // const query = 'INSERT INTO users (Name, Email, Password) VALUES (?, ?, ?)';
-    // const [result] = await db.execute(query, [Name, Email, Password]);
-    // db.release();
-    return NextResponse.json({
-        message: "User created successfully"
-    }, {status: 200});
-
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 import React, {useState} from 'react'
-import RegisterButton from '../componets/buttons/registerButton'
 import BackButton from '../componets/buttons/backButton'
 import Link from 'next/link'
+import {Dialog} from '@headlessui/react'
 
 const register = () => {
     const [regUsername,
@@ -11,22 +11,33 @@ const register = () => {
         setRegEmail] = useState('');
     const [regPassword,
         setRegPassword] = useState('');
+    const [regPass,
+        setRegPass] = useState('');
+    const [responseMessage,
+        setResponseMessage] = useState ("");
+    const [status,
+        setStatus] = useState (Number);
 
     const handleRegister = async() => {
         const response = await fetch('/api/users', {
 
             method: 'POST',
-            body: JSON.stringify({Name: regUsername, Email: regEmail, Password: regPassword}),
+            body: JSON.stringify({Name: regUsername, Email: regEmail, Password: regPassword, Password1: regPass}),
             headers: {
                 'Content-Type': 'application/json'
             }
-        })
+
+        });
+        const result = await response.json();
+        setStatus(response.status);
+        setResponseMessage(result.message);
 
     };
 
     return (
 
         <div className="bg-blue-200 h-screen">
+
             <div id='center'>
                 <div id='div1'>
                     <div className='input'>
@@ -39,7 +50,7 @@ const register = () => {
                     <br/>
                     <div className='input'>
                         <input
-                            type='email'
+                            type='text'
                             name='Email'
                             placeholder='Enter Email'
                             onChange={e => setRegEmail(e.target.value)}></input>
@@ -54,14 +65,29 @@ const register = () => {
                     </ div>
                     <br/>
                     <div className='input'>
-                        <input type='password' placeholder='Re-Enter Password'></input>
+                        <input
+                            type='password'
+                            name='Password1'
+                            placeholder='Enter Password'
+                            onChange={e => setRegPass(e.target.value)}></input>
                     </ div><br/>
 
-                    <Link href="/">
+                    <Link href="">
                         <button id='button1' onClick={handleRegister}>register</button>
                     </Link>
                     <br/>
                     <BackButton/>
+                    <br/>
+
+                    <div
+                        style={{
+                        color: status === 200
+                            ? 'green'
+                            : 'red'
+                    }}>
+                        {responseMessage}
+                    </div>
+
                 </div>
             </div>
 
