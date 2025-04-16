55e3cb19c7dd4ed553edbd76139798372fb12bf6
Christopher Ramos <chrisjay123890@gmail.com>
2025-04-15 21:47:45 -0400

    testing since I switched from azure to github

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,27 +1,24 @@
-import React from 'react'
-import NavBar from '../componets/navBar'
-import Link from 'next/link'
-
+import React from "react";
+import NavBar from "../componets/navBar";
+import Link from "next/link";
+("testing");
 const homePage = () => {
-    return (
-        <div className="bg-blue-200 flex flex-col h-screen justify-evenly">
-            <NavBar/>
+  return (
+    <div className="bg-blue-200 flex flex-col h-screen justify-evenly">
+      <NavBar />
 
-            <div className="m-auto">
-                <div >
-                    <div id='button1'>
-
-                        <Link href='/join'>
-                            <button>Join Team</button> 
-                        </Link>
-                    </div> <br/> <br/>
-                  
-                </div>
-            </div>
-
+      <div className="m-auto">
+        <div>
+          <div id="button1">
+            <Link href="/join">
+              <button>Join Team</button>
+            </Link>
+          </div>{" "}
+          <br /> <br />
         </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default homePage
+export default homePage;
